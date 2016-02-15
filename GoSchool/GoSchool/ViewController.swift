020912fcaa28c5fb5feffdc9776d0c2bf8ad854c
srachha <rachhasandeepkumar.4a3@gmail.com>
2016-02-15 19:37:17 +0530

    GoSchool Repo Created

---
+++ GoSchool/GoSchool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoSchool
+//
+//  Created by srachha on 26/11/1937 GREG.
+//  Copyright © 1937 Saka srachha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
